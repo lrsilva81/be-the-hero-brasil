05793748de92b85890de7833c615b3a1d1cf1ebc
lrsilva81 <leo.ribeiro.silva@gmail.com>
2020-06-24 14:20:42 +0100

    first commit

---
+++ src/database/connection.js
@@ -0,0 +1,8 @@
+const knex = require('knex');
+const configuration = require('../../knexfile');
+
+const config = process.env.NOD_ENV === 'test' ? configuration.test : configuration.development;
+
+const connection = knex(config);
+
+module.exports = connection;
